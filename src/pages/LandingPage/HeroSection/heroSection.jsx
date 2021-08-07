f290fb455841b1ef8761b9df21e2aef48dec552e
Shashwat <shashwatrathod22@gmail.com>
2021-08-07 12:27:06 +0530

    added section id

--- src/pages/LandingPage/HeroSection/heroSection.jsx
+++ src/pages/LandingPage/HeroSection/heroSection.jsx
@@ -17,62 +17,64 @@ const HeroSection = () => {
   ];
   return (
     <>
-      <div className="hero-container">
-        <div className="hero-bg" aria-hidden="true"></div>
-        <div className="container">
-          <div className="main-grid">
-            <div className="info-container">
-              <Slide top cascade>
-                <div className="name-container">
-                  <div className="filler-text">Hey there! I'm</div>
-                  <h1 className="name">Shashwat Rathod</h1>
-                </div>
-                <div className="adjective-container">
-                  <Typewriter
-                    options={{
-                      strings: whatAmIs,
-                      autoStart: true,
-                      delay: 50,
-                      deleteSpeed: 30,
-                      pauseFor: 1000,
-                      loop: true,
-                    }}
-                  />
-                </div>
-              </Slide>
-              <Slide bottom>
-                <div className="buttons-container">
-                  <PillButton aria-label="email">Get In Touch</PillButton>
-                  <PillButton aria-label="download cv">Dowload CV</PillButton>
+      <section id="home">
+        <div className="hero-container">
+          <div className="hero-bg" aria-hidden="true"></div>
+          <div className="container">
+            <div className="main-grid">
+              <div className="info-container">
+                <Slide top cascade>
+                  <div className="name-container">
+                    <div className="filler-text">Hey there! I'm</div>
+                    <h1 className="name">Shashwat Rathod</h1>
+                  </div>
+                  <div className="adjective-container">
+                    <Typewriter
+                      options={{
+                        strings: whatAmIs,
+                        autoStart: true,
+                        delay: 50,
+                        deleteSpeed: 30,
+                        pauseFor: 1000,
+                        loop: true,
+                      }}
+                    />
+                  </div>
+                </Slide>
+                <Slide bottom>
+                  <div className="buttons-container">
+                    <PillButton aria-label="email">Get In Touch</PillButton>
+                    <PillButton aria-label="download cv">Dowload CV</PillButton>
+                  </div>
+                </Slide>
+              </div>
+              <Slide right>
+                <div className="illustration-container" aria-hidden="true">
+                  <TypingIllustration className="illustration" />
                 </div>
               </Slide>
             </div>
-            <Slide right>
-              <div className="illustration-container" aria-hidden="true">
-                <TypingIllustration className="illustration" />
-              </div>
-            </Slide>
-          </div>
-          <div className="socials-container">
-            {socials.map((social, index) => (
-              <a
-                key={social.title}
-                href={social.link}
-                className="social-button"
-                target="_blank"
-                rel="noreferrer"
-                aria-label={social.title}
-                aria-labelledby={social.key}
-              >
-                {social.icon}
-                <div className="username" id={social.key}>
-                  {social.username}
-                </div>
-              </a>
-            ))}
+            <div className="socials-container">
+              {socials.map((social, index) => (
+                <a
+                  key={social.title}
+                  href={social.link}
+                  className="social-button"
+                  target="_blank"
+                  rel="noreferrer"
+                  aria-label={social.title}
+                  aria-labelledby={social.key}
+                >
+                  {social.icon}
+                  <div className="username" id={social.key}>
+                    {social.username}
+                  </div>
+                </a>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
+      </section>
     </>
   );
 };
