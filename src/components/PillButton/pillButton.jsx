4864b17e1eca26e395234eb5281c04348e533459
Shashwat <shashwatrathod22@gmail.com>
2021-08-21 18:10:35 +0530

    added ripple effect on btn click

--- src/components/PillButton/pillButton.jsx
+++ src/components/PillButton/pillButton.jsx
@@ -2,9 +2,39 @@ import React from "react";
 import "./pillButton.scss";
 
 const PillButton = (props) => {
+  const createRipples = (e) => {
+    const button = e.currentTarget;
+
+    const circle = document.createElement("span");
+    const diameter = Math.max(button.clientWidth, button.clientHeight);
+    const radius = diameter / 2;
+
+    circle.style.width = circle.style.height = `${diameter}px`;
+    const rect = button.getBoundingClientRect();
+    circle.style.left = `${e.clientX - rect.x - radius}px`;
+    circle.style.top = `${e.clientY - rect.y - radius}px`;
+
+    circle.classList.add("ripple");
+
+    const ripple = button.getElementsByClassName("ripple")[0];
+
+    if (ripple) {
+      ripple.remove();
+    }
+
+    button.appendChild(circle);
+  };
+
   return (
     <>
-      <button className="pillBtn">{props.children || props.title}</button>
+      <button
+        className="pillBtn"
+        onClick={(e) => {
+          createRipples(e);
+        }}
+      >
+        {props.children || props.title}
+      </button>
     </>
   );
 };
