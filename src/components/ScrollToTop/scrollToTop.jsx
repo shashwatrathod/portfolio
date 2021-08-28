48f3e33fef3a443e1e1ab4d1799015c35747b22e
Shashwat <shashwatrathod22@gmail.com>
2021-08-28 12:32:59 -0400

    shifted prev scroll to useRef

--- src/components/ScrollToTop/scrollToTop.jsx
+++ src/components/ScrollToTop/scrollToTop.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./scrollToTop.scss";
 import { IoIosArrowUp } from "react-icons/io";
 import clsx from "clsx";
@@ -6,7 +6,7 @@ import { animateScroll as scroll } from "react-scroll";
 
 const ScrollToTop = () => {
   const [visible, setVisible] = useState(false);
-  let prevScroll = 0;
+  let prevScroll = useRef(0);
 
   const onScrollToTopClick = () => {
     scroll.scrollToTop();
@@ -14,14 +14,14 @@ const ScrollToTop = () => {
   useEffect(() => {
     let toggleVisibility = () => {
       let st = window.pageYOffset;
-      if (st > 500 && st < prevScroll) {
+      if (st > 500 && st < prevScroll.current) {
         setVisible(true);
       } else {
         setVisible(false);
       }
 
       let tempScroll = st <= 0 ? 0 : st;
-      prevScroll = tempScroll;
+      prevScroll.current = tempScroll;
     };
 
     window.addEventListener("scroll", toggleVisibility);
